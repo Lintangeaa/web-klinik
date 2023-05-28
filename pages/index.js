11a659a5c752c851011927ebe3e725ff0bab1185
Lintangeaa <lindprak@gmail.com>
2023-05-28 16:16:05 +0700

    add

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import CarouselHome from '@/components/carousel/CarouselHome'
+import Image from "next/image"
+import CarouselHome from "@/components/carousel/CarouselHome"
 
 export default function Home() {
   return (
     <main className="">
-      <div className='w-full'>
+      <div className="w-full">
         <div className="border-t-4 border-gray-200">
           <CarouselHome />
-          <div className="absolute z-50 w-full max-w-xs top-full bg-slate-600 right-3/4 h-80 rounded-xl">
+          <div className="absolute z-50 w-full max-w-xs text-gray-600 bg-white cursor-pointer top-full right-3/4 h-80 rounded-xl hover:bg-primary hover:text-white">
             a
           </div>
           <div className="absolute z-50 w-full max-w-xs transform -translate-x-1/2 bg-white top-full left-1/2 h-80 rounded-xl">
@@ -21,5 +21,5 @@ export default function Home() {
 
       <div className="flex h-96"></div>
     </main>
-  );
+  )
 }
