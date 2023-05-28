9df2ac31c23931b1b2f7b2896c2faebb30cb259a
Lintangoao <prakosolintangdandung@gmail.com>
2023-05-28 16:13:23 +0700

    new change

--- components/Footer.js
+++ components/Footer.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react"
 
 const Footer = () => {
   return (
     <main>
       <div className="py-5 bg-secondary">
-        <div className="flex justify-between max-w-6xl mx-auto">
-          <p className='w-1/2'>Copyright © 2023 Kelompok 4 Reserved.</p>
-          <div className='w-1/2'>
-            <ul className='flex justify-end space-x-10 text-normal'>
-              <li>Privacy Policy</li>
-              <li>Contact</li>
+        <div className="flex justify-between max-w-6xl mx-auto font-semibold text-white">
+          <p className="w-1/2">Copyright © 2023 Kelompok 4 Reserved.</p>
+          <div className="w-1/2">
+            <ul className="flex justify-end space-x-10 text-normal">
+              <li>Privacy Policy</li>s<li>Contact</li>
               <li>Mbuhlah</li>
             </ul>
           </div>
         </div>
       </div>
     </main>
-  );
+  )
 }
 
 export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-import Image from 'next/image'
-import CarouselHome from '@/components/carousel/CarouselHome'
+import Image from "next/image"
+import CarouselHome from "@/components/carousel/CarouselHome"
 
 export default function Home() {
   return (
     <main className="">
-      <div className='w-full'>
+      <div className="w-full">
         <div className="border-t-4 border-gray-200">
           <CarouselHome />
-          <div className="absolute z-50 w-full max-w-xs top-full bg-slate-600 right-3/4 h-80 rounded-xl">
+          <div className="absolute z-50 w-full max-w-xs top-full bg-white shadow-lg right-3/4 h-80 hover:bg-primary hover:text-white transition ease-in-out delay-150 hover:-translate-y-8 duration-300 ...">
             a
           </div>
-          <div className="absolute z-50 w-full max-w-xs transform -translate-x-1/2 bg-white top-full left-1/2 h-80 rounded-xl">
+          <div className="absolute z-50 w-full max-w-xs transform -translate-x-1/2 bg-white shadow-lg top-full left-1/2 h-80 hover:bg-primary hover:text-white transition ease-in-out delay-150 hover:-translate-y-8 duration-300 ...">
             a
           </div>
-          <div className="absolute z-50 w-full max-w-xs bg-white top-full left-3/4 h-80 rounded-xl">
-            a
+          <div className="absolute z-50 w-full max-w-xs bg-white shadow-lg top-full left-3/4 h-80 hover:bg-primary hover:text-white transition ease-in-out delay-150 hover:-translate-y-8 duration-300 ...">
+            a``
           </div>
         </div>
       </div>
 
       <div className="flex h-96"></div>
     </main>
-  );
+  )
 }
