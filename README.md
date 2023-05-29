2a033589969c98f7a97c8c86c8031cbed28584e4
whxzuu <wahyubudiprayogo8@gmail.com>
2023-05-29 15:24:35 +0700

    add icon

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ pnpm dev
 
 ```
 
-## sddddYOWHATSAPPP
+## saaddddYOWHATSAPPP
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,26 @@
-import React from 'react'
-import {
-  SlLocationPin
-} from "react-icons/sl"
-import { MdOutlineAvTimer } from "react-icons/md"
-import Button from './Button'
-import DropdownNavbar from './dropdown/DropdownNavbar'
-import ContentData from '../store/ContentData'
+import React from "react";
+import { SlLocationPin } from "react-icons/sl";
+import { MdOutlineAvTimer } from "react-icons/md";
+import Button from "./Button";
+import DropdownNavbar from "./dropdown/DropdownNavbar";
+import ContentData from "../store/ContentData";
 
 const Header = () => {
-
   return (
     <div className="sticky z-50 w-screen text-gray-700 border-t-8 border-primary bg-slate-100 pb-7">
       <div className="flex justify-start max-w-6xl py-4 mx-auto space-x-3">
         <div className="flex space-x-2 cursor-pointer">
-          <SlLocationPin className='text-secondary'></SlLocationPin>
+          <SlLocationPin className="text-secondary"></SlLocationPin>
           <p className="text-sm">Alamat</p>
         </div>
         <div className="flex px-10 space-x-2 cursor-pointer">
           <MdOutlineAvTimer className="text-xl text-secondary"></MdOutlineAvTimer>
-          <p className="text-sm">Jam buka</p> 
+          <p className="text-sm">Jam buka</p>
         </div>
       </div>
 
       <div className="relative flex w-full max-w-6xl mx-auto bg-gray-200 border border-gray-300 rounded-full py-7">
-        <div className="flex justify-center w-1/3">Logo</div>
+        <div className="flex justify-center w-1/3"></div>
         <div className="flex justify-center w-1/3">
           <ul className="flex justify-between space-x-10 font-medium">
             <li className="cursor-pointer hover:text-primary">HOME</li>
@@ -49,6 +46,6 @@ const Header = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
